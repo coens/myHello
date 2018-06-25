9bed3327eeda1c36ba04e3180a579f695da90e71
coens <coens@imac.local>
2018-06-25 20:36:27 +0900

    Initial Commit

---
+++ myHello/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  myHello
+//
+//  Created by coens on 2018. 6. 25..
+//  Copyright © 2018년 coens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var sum = 0
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var count = 0
+        for row in 5...10 {
+            count += 1
+            self.sum += row
+        }
+        
+        print("총 합은 \(self.sum) / \(count)회 실행되었습니다.")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
